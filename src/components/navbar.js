4486b023ce38c36a58ca691b7dcefaf3a2320d1e
Ronak Vanpariya <vanpariyar@gmail.com>
2020-06-26 21:37:40 +0530

    Code optimized

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,20 +3,20 @@ import React from 'react'
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand">Navbar</a>
+            <a href="./" className="navbar-brand">Navbar</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                 <li className="nav-item active">
-                    <a className="nav-link">Home <span className="sr-only">(current)</span></a>
+                    <a href="#home" className="nav-link">Home <span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="">Features</a>
+                    <a href="https://github.com/vanpariyar/useful-tools/" className="nav-link" >Features</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link disabled">Disabled</a>
+                    <a href="#never" className="nav-link disabled">Disabled</a>
                 </li>
                 </ul>
             </div>
